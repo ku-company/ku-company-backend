35a2a943f272a2dc4d74c50f8488849f944bdf88
Natcha Ranchan <natcha.ran@ku.th>
2025-08-24 15:05:42 +0700

    set up connection

--- index.ts
+++ index.ts
@@ -2,9 +2,11 @@ import express from "express";
 import mockRouter  from "./router/mockRoutes.js";
 import cors from "cors";
 import type { Express , Request, Response }  from "express";
+import dotenv from "dotenv";
+import pool from "./config/db.js";
 
-
-const port = 8000;
+dotenv.config();
+const port = process.env.PORT || 8000;
 const app: Express = express();
 
 app.use(cors());
@@ -14,8 +16,41 @@ app.use("/api/mock", mockRouter);
 
 
 app.get("/", (req, res) => {
-  res.send("Server is running!");
+  res.send("Server is running ok!");
 });
+
+app.post("/test-db", async (req, res) => {
+  try {
+    await pool.query("CREATE TABLE test (id SERIAL PRIMARY KEY, name VARCHAR(100))");
+    res.send("Test table created");
+  } catch (err) {
+    console.error("Test query error:", err);
+    res.status(500).send("Test query failed");
+  }
+});
+
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM test");
+    res.send(result.rows);
+  } catch (err) {
+    console.error("Test query error:", err);
+    res.status(500).send("Test query failed");
+  }
+});
+
+// test connecting to db
+app.get("/setup", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT current_database()");
+    console.log("Connected to:", result.rows[0].current_database);
+    res.send(`Connected to DB: ${result.rows[0].current_database}`);
+  } catch (err) {
+    console.error("Connection error:", err);
+    res.status(500).send("Database connection failed");
+  }
+});
+
 
 // Start server
 app.listen(port, () => {
