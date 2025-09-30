b5555faf8dedc1b7996402a2b6bcbaa8709f59ff
Natcha Ranchan <natcha.ran@ku.th>
2025-09-30 22:38:48 +0700

    Filter only allow image uploading

--- middlewares/uploadMiddleware.ts
+++ middlewares/uploadMiddleware.ts
@@ -2,4 +2,13 @@ import multer from "multer";
 
 const storage = multer.memoryStorage();
 
-export const upload = multer({ storage: storage });
+export const upload = multer({ 
+    storage: storage ,
+    limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB cap 
+    fileFilter: (_req, file, cb) => {
+    if (!file.mimetype.startsWith("image/")) {
+      return cb(new Error("Only image uploads are allowed"));
+    }
+    cb(null, true);
+  },
+});
