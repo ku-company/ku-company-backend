4a3fc7ba389a3eb495ecbc0f9dd15f8550009e56
Napoldej Passornratchakul <napoldej.p@ku.th>
2025-11-07 23:26:05 +0700

    Get profile return email and profile image

--- helper/profileStrategy.ts
+++ helper/profileStrategy.ts
@@ -23,7 +23,9 @@ export class EmployeeProfile extends ProfileStrategy {
                     select: {
                         role: true,
                         first_name: true,
-                        last_name: true
+                        last_name: true,
+                        email:true,
+                        profile_image: true
                     }
                 }
             }
@@ -42,7 +44,9 @@ export class Professor extends ProfileStrategy {
             include: {
                 user: {
                     select: {
-                        role: true
+                        role: true,
+                        email: true,
+                        profile_image: true
                     }
                 }
             }
@@ -66,7 +70,10 @@ export class Company extends ProfileStrategy {
                 },
                 user: {
                     select: {
-                        role: true
+                        role: true,
+                        email: true,
+                        profile_image: true
+
                     }
                 }
             }
