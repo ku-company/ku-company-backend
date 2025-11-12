6b617db53b438a378190bdaa214a78747253ce54
Natcha Ranchan <natcha.ran@ku.th>
2025-11-12 11:03:44 +0700

    Remove Position enum validation function from validatorEnum.ts

--- utils/validatorEnum.ts
+++ utils/validatorEnum.ts
@@ -1,11 +1,7 @@
-import {JobType, Position, JobPostStatus} from "./enums.js";
+import {JobType, JobPostStatus} from "./enums.js";
 
 export function isJobType(val: any): val is JobType {
   return Object.values(JobType).includes(val as JobType);
-}
-
-export function isPosition(val: any): val is Position {
-  return Object.values(Position).includes(val as Position);
 }
 
 export function isJobPostStatus(val: any): val is JobPostStatus {
