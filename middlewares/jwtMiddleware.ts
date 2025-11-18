c5889fff9b1fd6bbfa309a255687298af1f392ea
Natcha Ranchan <natcha.ran@ku.th>
2025-11-19 00:24:44 +0700

    Refactor jwtMiddleware to check cookie after Authorization header

--- middlewares/jwtMiddleware.ts
+++ middlewares/jwtMiddleware.ts
@@ -5,13 +5,13 @@ const jwtMiddleware = expressjwt({
   algorithms: ["HS256"],
   requestProperty: "user",
   getToken: (req) => {
-    // Check cookie first
-    if (req.cookies && req.cookies.access_token) {
-      return req.cookies.access_token;
-    }
     // Fallback to Authorization header
     if (req.headers.authorization && req.headers.authorization.startsWith("Bearer ")) {
       return req.headers.authorization.split(" ")[1];
+    }
+    // Check cookie f
+    if (req.cookies && req.cookies.access_token) {
+      return req.cookies.access_token;
     }
     return null;
   }
